8a66727bc76f717b5456d96020cd9903c57af76f
Amrit Khadka <amrit.khadka@bloomsmobility.com>
2022-02-20 11:28:12 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insert(5);
+        list.insert(52);
+        list.insert(85);
+        list.insert(59);
+        list.show();
+    }
+}
